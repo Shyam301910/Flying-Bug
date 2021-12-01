d0b3107bd5bd9b3356a2fb23c4620415a0d265c2
akshitjain20bce1818 <88319107+akshitjain20bce1818@users.noreply.github.com>
2021-12-01 12:40:21 +0530

    Create getRandomPipe.py

---
+++ getRandomPipe.py
@@ -0,0 +1,43 @@
+    pipeHeight = GAME_SPRITES['pipe'][0].get_height()
+    offset = SCREENHEIGHT/3
+    y2 = offset + random.randrange(0, int(SCREENHEIGHT - GAME_SPRITES['base'].get_height()  - 1.2 *offset))
+    pipeX = SCREENWIDTH + 10
+    y1 = pipeHeight - y2 + offset
+    pipe = [
+        {'x': pipeX, 'y': -y1}, #upper Pipe
+        {'x': pipeX, 'y': y2} #lower Pipe
+    ]
+    return pipe
+if _name_ == "_main_":
+    # This will be the main point from where our game will start
+    pygame.init() # Initialize all pygame's modules
+    FPSCLOCK = pygame.time.Clock()
+    pygame.display.set_caption('BUG')
+    GAME_SPRITES['numbers'] = ( 
+        pygame.image.load('gallery/images/0.png').convert_alpha(),
+        pygame.image.load('gallery/images/1.png').convert_alpha(),
+        pygame.image.load('gallery/images/2.png').convert_alpha(),
+        pygame.image.load('gallery/images/3.png').convert_alpha(),
+        pygame.image.load('gallery/images/4.png').convert_alpha(),
+        pygame.image.load('gallery/images/5.png').convert_alpha(),
+        pygame.image.load('gallery/images/6.png').convert_alpha(),
+        pygame.image.load('gallery/images/7.png').convert_alpha(),
+        pygame.image.load('gallery/images/8.png').convert_alpha(),
+        pygame.image.load('gallery/images/9.png').convert_alpha(),
+    )
+    GAME_SPRITES['message'] =pygame.image.load('gallery/images/message.png').convert_alpha()
+    GAME_SPRITES['base'] =pygame.image.load('gallery/images/base.png').convert_alpha()
+    GAME_SPRITES['pipe'] =(pygame.transform.rotate(pygame.image.load( PIPE).convert_alpha(), 180), 
+    pygame.image.load(PIPE).convert_alpha()
+    )
+    # Game sounds
+    GAME_SOUNDS['die'] = pygame.mixer.Sound('gallery/audio/die.wav')
+    GAME_SOUNDS['hit'] = pygame.mixer.Sound('gallery/audio/hit.wav')
+    GAME_SOUNDS['point'] = pygame.mixer.Sound('gallery/audio/point.wav')
+    GAME_SOUNDS['swoosh'] = pygame.mixer.Sound('gallery/audio/swoosh.wav')
+    GAME_SOUNDS['wing'] = pygame.mixer.Sound('gallery/audio/wing.wav')
+    GAME_SPRITES['background'] = pygame.image.load(BACKGROUND).convert()
+    GAME_SPRITES['player'] = pygame.image.load(PLAYER).convert_alpha()
+    while True:
+        welcomeScreen() # Shows welcome screen to the user until he presses a button
+        mainGame() # This is the main game function
